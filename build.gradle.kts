2f0b6a2a499196c5753bc5499c1a36d38ecbbaa6
Ian Chen <ianchen7777@gmail.com>
2024-08-13 22:53:10 +1000

    Upgrade Kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "1.9.24"
-    kotlin("plugin.spring") version "1.9.24"
+    kotlin("jvm") version "2.0.10"
+    kotlin("plugin.spring") version "2.0.10"
     id("org.springframework.boot") version "3.3.2"
     id("io.spring.dependency-management") version "1.1.6"
     id("com.ncorti.ktfmt.gradle") version "0.19.0"
